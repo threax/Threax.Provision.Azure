776927f43ace64cada9194340d7f48eebaee647e
Andrew Piper <andrew@threax.com>
2021-05-13 15:02:37 -0400

    Add push file.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.Provision.Azure.git
+git push https://github.com/threax/Threax.Provision.Azure.git --tags
+git push https://gitlab.com/threax-software/Threax.Provision.Azure.git
+git push https://gitlab.com/threax-software/Threax.Provision.Azure.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Provision.Azure
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Provision.Azure --tags
