e84f5a8473848a18aececf060a6fa8f1b626d3d2
Andrew Piper <andrew@threax.com>
2021-05-13 11:33:59 -0400

    Added exec and acr login controllers.

---
+++ Threax.AzureVmProvisioner/Controller/ExecController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Threax.AzureVmProvisioner.Resources;
+using Threax.AzureVmProvisioner.Services;
+using Threax.ConsoleApp;
+using Threax.DockerBuildConfig;
+using Threax.ProcessHelper;
+
+namespace Threax.AzureVmProvisioner.Controller
+{
+    record ExecController
+    (
+        IVmCommands vmCommands,
+        IPathHelper pathHelper,
+        IArgsProvider argsProvider,
+        ResourceConfiguration resources
+    )
+    : IController
+    {
+        public async Task Run()
+        {
+            var resource = resources.Compute;
+
+            if (String.IsNullOrEmpty(resource.Name))
+            {
+                throw new InvalidOperationException("You must include a resource 'Name' property to deploy compute.");
+            }
+
+            var command = argsProvider.Args[2];
+            var args = argsProvider.Args.Skip(3).ToArray();
+            var fileName = Path.GetFileName(pathHelper.ConfigPath);
+            await vmCommands.ThreaxDockerToolsExec($"/app/{resource.Name}/{fileName}", command, args);
+        }
+    }
+}
---
+++ Threax.AzureVmProvisioner/Controller/LoginAcrController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Threax.ProcessHelper;
+using Threax.Provision.AzPowershell;
+
+namespace Threax.AzureVmProvisioner.Controller
+{
+    record LoginAcrController 
+    (
+        EnvironmentConfiguration config, 
+        IAcrManager acrManager, 
+        IShellRunner shellRunner,
+        ILogger<LoginAcrController> logger
+    )
+    : IController
+    {
+        public async Task Run()
+        {
+            logger.LogInformation($"Logging into ACR '{config.AcrName}'.");
+
+            var acrCreds = await acrManager.GetAcrCredential(config.AcrName, config.ResourceGroup);
+
+            var passwordPath = Path.GetTempFileName();
+
+            try
+            {
+                using (var passwordStream = new StreamWriter(File.Open(passwordPath, FileMode.Create, FileAccess.ReadWrite, FileShare.None)))
+                {
+                    passwordStream.Write(acrCreds.Password);
+                }
+
+                var acrLoginUrl = $"{config.AcrName}.azurecr.io";
+
+                shellRunner.RunProcessVoid($"cat {passwordPath} | docker login {acrLoginUrl} --username {acrCreds.Username} --password-stdin", invalidExitCodeMessage: $"Error logging into ACR '{config.AcrName}'.");
+            }
+            finally
+            {
+                //Erase the file
+                if (File.Exists(passwordPath))
+                {
+                    File.Delete(passwordPath);
+                }
+            }
+        }
+    }
+}
