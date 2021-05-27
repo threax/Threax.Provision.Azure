c561ef761259006a1982a4492d8389355b336c73
Andrew Piper <andrew@threax.com>
2021-05-27 13:12:06 -0400

    Added a way to destroy volumes.

---
+++ Threax.AzureVmProvisioner/Controller/DestroyWorkVolumesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Threax.ProcessHelper;
+
+namespace Threax.AzureVmProvisioner.Controller
+{
+    record DestroyWorkVolumesController
+    (
+        ILogger<DestroyWorkVolumesController> logger, 
+        IShellRunner shellRunner
+    ) : IController
+    {
+        public Task Run()
+        {
+            shellRunner.RunProcessVoid($"docker volume remove threax-provision-azurevm-home", "An error occured removing home volume.");
+            shellRunner.RunProcessVoid($"docker volume remove threax-provision-azurevm-temp", "An error occured removing temp volume.");
+
+            return Task.CompletedTask;
+        }
+    }
+}
