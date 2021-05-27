f44553a8840110a7d800377ade2fed8b1e84c96e
Andrew Piper <andrew@threax.com>
2021-05-27 13:08:07 -0400

    Added volumes to serve as the root and temp dirs when running the program.

---
+++ Threax.AzureVmProvisioner/Controller/CreateWorkVolumesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Threax.ProcessHelper;
+
+namespace Threax.AzureVmProvisioner.Controller
+{
+    record CreateWorkVolumesController
+    (
+        ILogger<CreateWorkVolumesController> logger, 
+        IShellRunner shellRunner
+    ) : IController
+    {
+        public Task Run()
+        {
+            shellRunner.RunProcessVoid($"docker volume create threax-provision-azurevm-home", "An error occured creating home volume.");
+            shellRunner.RunProcessVoid($"docker volume create threax-provision-azurevm-temp", "An error occured creating temp volume.");
+
+            return Task.CompletedTask;
+        }
+    }
+}
